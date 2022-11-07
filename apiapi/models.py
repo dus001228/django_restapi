51ecdf3372580662f6b8add56e0223c83e667999
dus001228 <dus001228@naver.com>
2022-11-07 23:46:59 +0900

    Update models.py

--- apiapi/models.py
+++ apiapi/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 class daejeonfood(models.Model):
-    번호 = models.Index = models.AutoField(primary_key=True)
-    업소명 = models.TextField()
-    도로명주소 = models.TextField()
+    id = models.Index = models.AutoField(primary_key=True)
+    title = models.TextField()
+    new_add = models.TextField()
     소재지주소 = models.TextField()
     지정일자 = models.BigIntegerField()
     음식의유형 = models.TextField()
     주된음식종류 = models.TextField()
-    전화번호 = models.TextField()
+    call = models.TextField()
+    img_url = models.TextField()
+
     
 
     def __str__(self):
