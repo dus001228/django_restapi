aa8687d9c5fec94d7fd5a6e889ddbd8510a4461f
dus001228 <dus001228@naver.com>
2022-11-07 23:46:42 +0900

    Update serializers.py

--- apiapi/serializers.py
+++ apiapi/serializers.py
@@ -4,4 +4,4 @@ from .models import daejeonfood
 class daejeonapiSerializer(serializers.ModelSerializer):
     class Meta:
         model = daejeonfood # 모델 설정
-        fields = ('id', '업소명', '도로명주소', '소재지주소', '지정일자', '음식의유형', '주된음식종류', '전화번호')
+        fields = ('id', 'title', 'new_add', '소재지주소', '지정일자', '음식의유형', '주된음식종류', 'call', 'img_url')
