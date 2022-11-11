418db3e794535ff30988cc446a2e6ae2b8a6d400
dus001228 <dus001228@naver.com>
2022-11-11 18:00:59 +0900

    Update urls.py

--- apitest/urls.py
+++ apitest/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path,include
 from django.contrib import admin
 from rest_framework import routers
-from apiapi.views import daejeonapiViewSet
+from apiapi.views import *
 
 router = routers.DefaultRouter() 
 router.register('foods',daejeonapiViewSet)
+router.register('news',daejeonnewsViewSet)
+router.register('weather',daejeonweatherViewSet)
+router.register('tour',daejeontourViewSet)
+router.register('rest',daejeonrestViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
