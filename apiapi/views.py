f5b6668a8b0c1885de34ad8e56e3dda05fa566ce
dus001228 <dus001228@naver.com>
2022-11-11 18:00:39 +0900

    Update views.py

--- apiapi/views.py
+++ apiapi/views.py
@@ -1,8 +1,24 @@
 from telnetlib import STATUS
 from rest_framework import viewsets
-from .serializers import daejeonapiSerializer
-from .models import daejeonfood
+from .serializers import *
+from .models import *
 
 class daejeonapiViewSet(viewsets.ModelViewSet):
     queryset = daejeonfood.objects.all()
     serializer_class = daejeonapiSerializer
+
+class daejeonnewsViewSet(viewsets.ModelViewSet):
+    queryset = daejeonnews.objects.all()
+    serializer_class = daejeonnewsSerializer
+
+class daejeonweatherViewSet(viewsets.ModelViewSet):
+    queryset = daejeonweather.objects.all()
+    serializer_class = daejeonweatherSerializer
+
+class daejeontourViewSet(viewsets.ModelViewSet):
+    queryset = daejeontour.objects.all()
+    serializer_class = daejeontourSerializer
+
+class daejeonrestViewSet(viewsets.ModelViewSet):
+    queryset = daejeonrest.objects.all()
+    serializer_class = daejeonrestSerializer
